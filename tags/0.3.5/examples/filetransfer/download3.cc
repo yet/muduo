14c9d4eab58006c48d8296b8adfcfa18a892772c
giantchen <giantchen@8438fe5c-22df-11df-a5e4-cb4ff25536bb>
2012-05-17 15:02:17 +0000

    Tagging 0.3.5 of muduo.

---
+++ tags/0.3.5/examples/filetransfer/download3.cc
@@ -0,0 +1,80 @@
+#include <muduo/base/Logging.h>
+#include <muduo/net/EventLoop.h>
+#include <muduo/net/TcpServer.h>
+
+#include <boost/shared_ptr.hpp>
+
+#include <stdio.h>
+
+using namespace muduo;
+using namespace muduo::net;
+
+const int kBufSize = 64*1024;
+const char* g_file = NULL;
+typedef boost::shared_ptr<FILE> FilePtr;
+
+void onConnection(const TcpConnectionPtr& conn)
+{
+  LOG_INFO << "FileServer - " << conn->peerAddress().toIpPort() << " -> "
+           << conn->localAddress().toIpPort() << " is "
+           << (conn->connected() ? "UP" : "DOWN");
+  if (conn->connected())
+  {
+    LOG_INFO << "FileServer - Sending file " << g_file
+             << " to " << conn->peerAddress().toIpPort();
+
+    FILE* fp = ::fopen(g_file, "rb");
+    if (fp)
+    {
+      FilePtr ctx(fp, ::fclose);
+      conn->setContext(ctx);
+      char buf[kBufSize];
+      size_t nread = ::fread(buf, 1, sizeof buf, fp);
+      conn->send(buf, nread);
+    }
+    else
+    {
+      conn->shutdown();
+      LOG_INFO << "FileServer - no such file";
+    }
+  }
+}
+
+void onWriteComplete(const TcpConnectionPtr& conn)
+{
+  FilePtr fp = boost::any_cast<FilePtr>(conn->getContext());
+  char buf[kBufSize];
+  size_t nread = ::fread(buf, 1, sizeof buf, get_pointer(fp));
+  if (nread > 0)
+  {
+    conn->send(buf, nread);
+  }
+  else
+  {
+    conn->shutdown();
+    LOG_INFO << "FileServer - done";
+  }
+}
+
+
+int main(int argc, char* argv[])
+{
+  LOG_INFO << "pid = " << getpid();
+  if (argc > 1)
+  {
+    g_file = argv[1];
+
+    EventLoop loop;
+    InetAddress listenAddr(2021);
+    TcpServer server(&loop, listenAddr, "FileServer");
+    server.setConnectionCallback(onConnection);
+    server.setWriteCompleteCallback(onWriteComplete);
+    server.start();
+    loop.loop();
+  }
+  else
+  {
+    fprintf(stderr, "Usage: %s file_for_downloading\n", argv[0]);
+  }
+}
+
